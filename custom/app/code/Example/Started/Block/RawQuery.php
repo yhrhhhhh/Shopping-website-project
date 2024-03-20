b7e946c39d3983d68daee21b9ac174bee0eb7f6e
yhr <yanhuiru81@gmail.com>
2024-03-20 13:12:30 +0800

    feat #10:create rawquery php

---
+++ custom/app/code/Example/Started/Block/RawQuery.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Example\Started\Block;
+
+use Magento\Framework\View\Element\Template;
+use Magento\Framework\App\ResourceConnection;
+
+class RawQuery extends Template
+{
+    protected $connection;
+
+    public function __construct(
+        Template\Context $context,
+        ResourceConnection $resource,
+        array $data = []
+    ) {
+        $this->connection = $resource->getConnection();
+        parent::__construct($context, $data);
+    }
+
+    public function getQueryResults()
+    {
+        $select = $this->connection->select()
+            ->from('catalog_category_entity')
+            ->columns(['entity_id', 'path'])
+            ->order('entity_id DESC')
+            ->limit(20);
+
+        return $this->connection->fetchAll($select);
+    }
+}
