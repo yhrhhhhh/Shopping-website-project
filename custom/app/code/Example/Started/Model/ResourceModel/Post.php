a192cd19aa9f64c6bd4c7d93adc711d1cae4c100
yhr <yanhuiru81@gmail.com>
2024-03-22 09:31:43 +0800

    create Post Model

---
+++ custom/app/code/Example/Started/Model/ResourceModel/Post.php
@@ -0,0 +1,20 @@
+<?php
+namespace Example\Started\Model\ResourceModel;
+
+
+class Post extends \Magento\Framework\Model\ResourceModel\Db\AbstractDb
+{
+
+    public function __construct(
+        \Magento\Framework\Model\ResourceModel\Db\Context $context
+    )
+    {
+        parent::__construct($context);
+    }
+
+    protected function _construct()
+    {
+        $this->_init('get_started', 'post_id');
+    }
+
+}
