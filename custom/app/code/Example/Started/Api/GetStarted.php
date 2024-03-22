91c092db98b77172dc8d5eb78df7148307b3b314
yhr <yanhuiru81@gmail.com>
2024-03-22 09:27:19 +0800

    create ResourceModel GetStarted

---
+++ custom/app/code/Example/Started/Api/GetStarted.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Example\Started\Api;
+
+use Example\Started\Model\ResourceModel\Post\Collection;
+
+class GetStarted implements GetStartedInterface
+{
+    protected $collection;
+
+    public function __construct(Collection $collection)
+    {
+        $this->collection = $collection;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAllRecords()
+    {
+        return $this->collection->getItems();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAllRecordsArray()
+    {
+        return $this->collection->toArray();
+    }
+}
