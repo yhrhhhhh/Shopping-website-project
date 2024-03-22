6674584aeb5ce71c5fc020a59ba0c45366e27c04
yhr <yanhuiru81@gmail.com>
2024-03-22 13:16:25 +0800

    ♻️ Return results early

--- custom/app/code/Example/Started/Setup/InstallSchema.php
+++ custom/app/code/Example/Started/Setup/InstallSchema.php
@@ -2,99 +2,118 @@
 
 namespace Example\Started\Setup;
 
-class InstallSchema implements \Magento\Framework\Setup\InstallSchemaInterface
+use Magento\Framework\DB\Adapter\AdapterInterface;
+use Magento\Framework\DB\Ddl\Table;
+use Magento\Framework\Setup\InstallSchemaInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\SchemaSetupInterface;
+
+class InstallSchema implements InstallSchemaInterface
 {
 
-    public function install(\Magento\Framework\Setup\SchemaSetupInterface $setup, \Magento\Framework\Setup\ModuleContextInterface $context)
+    public function install(SchemaSetupInterface $setup, ModuleContextInterface $context)
     {
-        $installer = $setup;
-        $installer->startSetup();
-        if (!$installer->tableExists('get_started')) {
-            $table = $installer->getConnection()->newTable(
-                $installer->getTable('get_started')
-            )
-                ->addColumn(
-                    'post_id',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-                    null,
-                    [
-                        'identity' => true,
-                        'nullable' => false,
-                        'primary' => true,
-                        'unsigned' => true,
-                    ],
-                    'Post ID'
-                )
-                ->addColumn(
-                    'name',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    255,
-                    ['nullable => false'],
-                    'Post Name'
-                )
-                ->addColumn(
-                    'url_key',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    255,
-                    [],
-                    'Post URL Key'
-                )
-                ->addColumn(
-                    'post_content',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    '64k',
-                    [],
-                    'Post Post Content'
-                )
-                ->addColumn(
-                    'tags',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    255,
-                    [],
-                    'Post Tags'
-                )
-                ->addColumn(
-                    'status',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_INTEGER,
-                    1,
-                    [],
-                    'Post Status'
-                )
-                ->addColumn(
-                    'featured_image',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_TEXT,
-                    255,
-                    [],
-                    'Post Featured Image'
-                )
-                ->addColumn(
-                    'created_at',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-                    null,
-                    ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT],
-                    'Created At'
-                )->addColumn(
-                    'updated_at',
-                    \Magento\Framework\DB\Ddl\Table::TYPE_TIMESTAMP,
-                    null,
-                    ['nullable' => false, 'default' => \Magento\Framework\DB\Ddl\Table::TIMESTAMP_INIT_UPDATE],
-                    'Updated At')
-                ->setComment('Post Table');
-            $installer->getConnection()->createTable($table);
+        $tableName = 'get_started';
 
-            $installer->getConnection()->addIndex(
-                $installer->getTable('get_started'),
+        if ($setup->tableExists($tableName)) {
+            echo 'Table exists!';
+            return;
+        }
+        $setup->startSetup();
+
+        $table = $setup->getConnection()
+            ->newTable($tableName)
+            ->addColumn(
+                'post_id',
+                Table::TYPE_INTEGER,
+                null,
+                [
+                    'identity' => true,
+                    'nullable' => false,
+                    'primary' => true,
+                    'unsigned' => true,
+                ],
+                'Post ID'
+            )
+            ->addColumn(
+                'name',
+                Table::TYPE_TEXT,
+                255,
+                ['nullable => false'],
+                'Post Name'
+            )
+            ->addColumn(
+                'url_key',
+                Table::TYPE_TEXT,
+                255,
+                [],
+                'Post URL Key'
+            )
+            ->addColumn(
+                'post_content',
+                Table::TYPE_TEXT,
+                '64k',
+                [],
+                'Post Post Content'
+            )
+            ->addColumn(
+                'tags',
+                Table::TYPE_TEXT,
+                255,
+                [],
+                'Post Tags'
+            )
+            ->addColumn(
+                'status',
+                Table::TYPE_INTEGER,
+                1,
+                [],
+                'Post Status'
+            )
+            ->addColumn(
+                'featured_image',
+                Table::TYPE_TEXT,
+                255,
+                [],
+                'Post Featured Image'
+            )
+            ->addColumn(
+                'created_at',
+                Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default' => Table::TIMESTAMP_INIT],
+                'Created At'
+            )->addColumn(
+                'updated_at',
+                Table::TYPE_TIMESTAMP,
+                null,
+                ['nullable' => false, 'default' => Table::TIMESTAMP_INIT_UPDATE],
+                'Updated At')
+            ->setComment('Post Table')
+            ->addIndex(
+                $tableName,
                 $setup->getIdxName(
-                    $installer->getTable('get_started'),
+                    $tableName,
                     ['name', 'url_key', 'post_content', 'tags', 'featured_image'],
-                    \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+                    AdapterInterface::INDEX_TYPE_FULLTEXT
                 ),
                 ['name', 'url_key', 'post_content', 'tags', 'featured_image'],
-                \Magento\Framework\DB\Adapter\AdapterInterface::INDEX_TYPE_FULLTEXT
+                AdapterInterface::INDEX_TYPE_FULLTEXT
             );
-        } else {
-            echo 'Table exists!';
-        }
-        $installer->endSetup();
+
+        $setup->getConnection()->createTable($table);
+
+        $setup->getConnection()->addIndex(
+            $tableName,
+            $setup->getIdxName(
+                $tableName,
+                ['name', 'url_key', 'post_content', 'tags', 'featured_image'],
+                AdapterInterface::INDEX_TYPE_FULLTEXT
+            ),
+            ['name', 'url_key', 'post_content', 'tags', 'featured_image'],
+            AdapterInterface::INDEX_TYPE_FULLTEXT
+        );
+
+        $setup->endSetup();
     }
 }
